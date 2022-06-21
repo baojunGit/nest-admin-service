194b6de192675348b320c01f8570acd133b33436
baojunGit <1018440268@qq.com>
2022-06-21 11:20:07 +0800

    fix: 解决nanoid V4版本不兼容commonjs的问题，回退到v3版本

--- src/modules/system/auth/auth.service.ts
+++ src/modules/system/auth/auth.service.ts
@@ -5,6 +5,8 @@ import { User } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
 import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
+// 这里nanoid要使用V3版本，不然会报错
+// V4不支持 CommonJS
 import { nanoid } from 'nanoid';
 
 @Injectable()
@@ -24,6 +26,7 @@ export class AuthService {
   async createCaptcha() {
     const svg = await this.captchaService.getCaptcha();
     console.log(svg);
+    const uuid = nanoid(8);
     // Buffer是node里的一个模块，这个模块的出现是因为js没有阅读和操作二进制数据流而出现的
     // Buffer.from()从字符串或者数组创建一个buffer，将内容写入刚刚申请的内存中
     const result = {
@@ -31,10 +34,15 @@ export class AuthService {
         'base64',
       )}`,
       // 生成一个唯一的id标识，方便查询
-      id: nanoid(),
+      id: uuid,
     };
     // 5分钟过期时间
-    await this.redis.set('tag', svg.text, 'EX', 60 * 5);
+    await this.redis.set(
+      `admin:captcha:img:${result.id}`,
+      svg.text,
+      'EX',
+      60 * 5,
+    );
     return result;
   }
 
